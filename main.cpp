eff175e23d8092d9629715d0cb9d866462204bbc
jkatz94 <jkatz94@gmail.com>
2015-09-18 10:25:44 -0400

    Added clock output and cleaned up a bit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <algorithm>
+#include <ctime>
 
 #include "Hash.h"
 
@@ -22,6 +23,33 @@ int main() {
 	dictionaryName = "wordlist_small.sdx"; // TEMP LINE
 	cout << dictionaryName << endl; // TEMP LINE
 
+	// Load dictionary into hash table
+	hashTable dictionary;
+
+	clock_t t1 = clock();
+	
+	// Open dictionary file
+	ifstream dictionaryFile(dictionaryName.c_str());
+	if (!dictionaryFile) {
+		cout << "Error opening dictionary file" << endl;
+		exit(1);
+	}
+
+	// Insert each word into dictionary hash table
+	string line;
+	while (getline(dictionaryFile, line)) {
+		// Make word lower case, then insert into dictionary
+		transform(line.begin(), line.end(), line.begin(), ::tolower);
+		dictionary.insert(line);
+	}
+
+	// Close dictionary file
+	dictionaryFile.close();
+
+	clock_t t2 = clock();
+	double timeDiff = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
+	cout << "Total time (in seconds) to load dictionary: " << timeDiff << endl;
+
 	cout << "Enter an input filename: ";
 	//cin >> inputName;
 	inputName = "lyrics.txt"; // TEMP LINE
@@ -29,24 +57,10 @@ int main() {
 
 	cout << "Enter an output filename: ";
 	//cin >> outputName;
-	outputName = "lyrics.txt"; // TEMP LINE
+	outputName = "output.txt"; // TEMP LINE
 	cout << outputName << endl; // TEMP LINE
 
-	// Load dictionary into hash table
-	hashTable dictionary;
-
-	ifstream dictionaryFile(dictionaryName.c_str());
-	if (!dictionaryFile) {
-		cout << "Error opening dictionary file" << endl;
-		exit(1);
-	}
-
-	string line;
-	while (getline(dictionaryFile, line)) {
-		// Make word lower case, then insert into dictionary
-		transform(line.begin(), line.end(), line.begin(), ::tolower);
-		dictionary.insert(line);
-	}
+	clock_t t3 = clock();
 
 	// Open input file
 	ifstream inputFile(inputName.c_str());
@@ -55,6 +69,14 @@ int main() {
 		exit(1);
 	}
 
+	// Open output file
+	ofstream outputFile(outputName.c_str());
+	if (!outputFile) {
+		cout << "Error opening output file" << endl;
+		exit(1);
+	}
+
+	// Spellcheck vars
 	char * current = new char[1];
 	string word = "";
 	int lineNumber = 1;
@@ -74,27 +96,26 @@ int main() {
 		// On file end, add char manually and ensure that wordLetter returns false
 		if (inputFile.peek() == EOF) {
 			word += *current;
-			*current = '\0';
+			*current = '\0'; // Change to separator character to wordLetter returns false
 		}
 
 		if (wordLetter(*current)) {
+			// If current is still a word letter, add to the current word
+			// And set hasNumber condition if applicable
 			word += *current;
 			if (*current >= '0' && *current <= '9') {
 				hasNumber = true;
 			}
 		} else {
+			// If current is a separator, spell check the word if it exists and doesn't contain a number
 			if (word != "") {
 				// Don't spellcheck if word contains number
 				if (!hasNumber) {
-					// Dump out contains data
-					cout << lineNumber << "\t" << word << "\t";
 					int wordStatus = checkWord(word, dictionary);
-					if (wordStatus == 0) {
-						cout << "Nothing wrong" << endl;
-					} else if (wordStatus == 1) {
-						cout << "Too long" << endl;
+					if (wordStatus == 1) {
+						outputFile << "Long word at line " << lineNumber << ", starts: " << word.substr(0, 20) << "\n";
 					} else if (wordStatus == 2) {
-						cout << "Not in dictionary" << endl;
+						outputFile << "Unknown word at line " << lineNumber << ": " << word << "\n";
 					}
 				} else {
 					hasNumber = false;
@@ -109,7 +130,13 @@ int main() {
 		}
 	}
 
-	cout << "Done reading" << endl;
+	// Close input and output files
+	inputFile.close();
+	outputFile.close();
+
+	clock_t t4 = clock();
+	timeDiff = ((double) (t4 - t3)) / CLOCKS_PER_SEC;
+	cout << "Total time (in seconds) to check document: " << timeDiff << endl;
 
     system("pause");
 	return 0;
